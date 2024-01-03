413ef346d83af34cf504f10579f581e09fb91aae
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2024-01-03 14:52:53 -0300

    projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine(" Nokia:");
+Smartphone nokia = new Nokia("julius number", "model julius", "123456789", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("freefire");
+
+Console.WriteLine("\n Iphone");
+Smartphone iphone = new Iphone("Julia number", "model julia", "987654321", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("mario cart");
